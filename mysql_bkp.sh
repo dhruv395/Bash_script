22e6458939e98ff321e4e2efac07dffddd919eea
Dhruba Jyoti Ray <36419707+dhruv395@users.noreply.github.com>
2020-09-07 21:33:08 +0530

    Create mysql_bkp.sh

---
+++ mysql_bkp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+date=$(date +\%d-\%m-\%y_\%H-\%M-\%S)
+bkp_dir="/home/ubuntu/backup/mysql"
+
+echo "taking mysql bkp"
+cd  $bkp_dir
+sudo mysqldump  -u root -p<password> --lock-tables=false camattendance | gzip > db1_$date.sql.gz
+sudo mysqldump -u root -p<password> --lock-tables=false  blog | gzip >  db2_$date.sql.gz
+
+echo "removing old backup"
+find . -type f -iname \*.gz -mtime +14 -exec rm -rf {} \;
