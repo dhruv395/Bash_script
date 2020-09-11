849ba6bc6c4b949693cce544be8ae5a382f0eb24
Dhruba Jyoti Ray <36419707+dhruv395@users.noreply.github.com>
2020-09-11 09:32:59 +0530

    Update mysql_bkp.sh

--- mysql_bkp.sh
+++ mysql_bkp.sh
@@ -4,8 +4,8 @@ bkp_dir="/home/ubuntu/backup/mysql"
 
 echo "taking mysql bkp"
 cd  $bkp_dir
-sudo mysqldump  -u root -p<password> --lock-tables=false camattendance | gzip > db1_$date.sql.gz
-sudo mysqldump -u root -p<password> --lock-tables=false  blog | gzip >  db2_$date.sql.gz
+sudo mysqldump  -u root -p<password> --lock-tables=false <dbname> | gzip > db1_$date.sql.gz
+sudo mysqldump -u root -p<password> --lock-tables=false  <dbname> | gzip >  db2_$date.sql.gz
 
 echo "removing old backup"
 find . -type f -iname \*.gz -mtime +14 -exec rm -rf {} \;
